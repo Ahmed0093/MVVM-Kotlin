238a52c6a500eb93414a75168aaf1cde1ca8afc6
Ahmed0093 <Ahmed0093@github.com>
2019-04-17 01:40:32 +0200

    Remove unUsed function

--- app/src/main/java/com/development/task/mvvmproductapp/testing/DummyData.kt
+++ app/src/main/java/com/development/task/mvvmproductapp/testing/DummyData.kt
@@ -9,22 +9,19 @@ import com.development.task.mvvmproductapp.data.local.Products
 
 @VisibleForTesting(otherwise = VisibleForTesting.NONE)
 object DummyData {
-    fun getDummy() : Products {
-                val products: Products
+    fun getDummy(): Products {
+        val products: Products
         var data: List<ProductModel> = emptyList()
         val image: PImage = PImage("http://dummyimage.com/150x178.png/5fa2dd/ffffff", "150", "120")
 
-        val itemdata = ProductModel(1,"title1", "description1", 6, image)
-        val itemdata2 = ProductModel(2, "title2", "descropionnn2", 7,  image)
+        val itemdata = ProductModel(1, "title1", "description1", 6, image)
+        val itemdata2 = ProductModel(2, "title2", "descropionnn2", 7, image)
 
         data += itemdata
 
         data += itemdata2
         products = Products(data, 10)
 
-      return products
+        return products
     }
-    fun <T : Any?> MutableLiveData<T>.default(initialValue: T) = apply { setValue(initialValue) }
-
-    val liveData = MutableLiveData<String>().default("Initial value!")
 }
